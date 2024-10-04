91d9eb9895ea380ded766f3dff8ad7747e054c94
Mohamed Outabachi <outabachi-mohamed@hotmail.fr>
2024-10-04 23:12:55 +0200

    premiere partie du responsive

--- index.js
+++ index.js
@@ -65,3 +65,58 @@ document.addEventListener('DOMContentLoaded', function () {
         showImage(currentIndex);
     });
 });
+
+    function ajouterBr() {
+        const pointInsertion = document.getElementById('insertion-point');
+        const screenWidth = window.innerWidth;
+
+        // Vérifie si l'écran fait moins de 640 pixels
+        if (screenWidth < 640) {
+            // Vérifie si un <br> n'a pas déjà été ajouté
+            if (!document.getElementById('saut-de-ligne')) {
+                // Crée une balise <br>
+                const br = document.createElement('br');
+                br.id = 'saut-de-ligne'; // Ajoute un ID pour identifier le <br> ajouté
+                pointInsertion.parentNode.insertBefore(br, pointInsertion);
+            }
+        } else {
+            // Si l'écran est plus large que 640px, supprime le <br> si existant
+            const br = document.getElementById('saut-de-ligne');
+            if (br) {
+                br.remove();
+            }
+        }
+    }
+
+    function ajouterBr2() {
+        const pointInsertion2 = document.getElementById('insertion-point2');
+        const screenWidth2 = window.innerWidth;
+
+        // Vérifie si l'écran fait moins de 640 pixels
+        if (screenWidth2 < 640) {
+            // Vérifie si un <br> n'a pas déjà été ajouté
+            if (!document.getElementById('saut-de-ligne2')) {
+                // Crée une balise <br>
+                const br2 = document.createElement('br');
+                br2.id = 'saut-de-ligne2'; // Ajoute un ID pour identifier le <br> ajouté
+                pointInsertion2.parentNode.insertBefore(br2, pointInsertion2);
+            }
+        } else {
+            // Si l'écran est plus large que 640px, supprime le <br> si existant
+            const br2 = document.getElementById('saut-de-ligne2');
+            if (br2) {
+                br2.remove();
+            }
+        }
+    }
+
+    function gérerResizeEtLoad() {
+        ajouterBr();
+        ajouterBr2();
+    }
+    
+    // Exécute la fonction à chaque redimensionnement de l'écran
+    window.addEventListener('resize', gérerResizeEtLoad);
+    
+    // Exécute la fonction au chargement initial de la page
+    window.addEventListener('load', gérerResizeEtLoad);
